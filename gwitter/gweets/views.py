bf85c6454529cf5559df5d237c51e757cab5f5a9
Chris Bartos <bartos.25@osu.edu>
2017-02-18 13:22:51 -0500

    Allowing Different permissions for list / retrieve actions on Gweets

--- gwitter/gweets/views.py
+++ gwitter/gweets/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from .models import Gweet
 from rest_framework import viewsets
 from .serializers import GweetSerializer
+from rest_framework.permissions import AllowAny
 
 class GweetViewSet(viewsets.ModelViewSet):
     queryset = Gweet.objects.all().order_by('-created_on')
     serializer_class = GweetSerializer
+
+    permission_classes_by_action = { 'list': (AllowAny,),
+                                     'retrieve': (AllowAny,)}
+
+    def get_permissions(self):
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except:
+            return [permission() for permission in self.permission_classes]
